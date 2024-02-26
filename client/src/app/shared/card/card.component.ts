f2e115023a11896f95354bc76276063d54c5e921
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-02-26 03:01:53 +0200

    card

---
+++ client/src/app/shared/card/card.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input } from '@angular/core';
+import { Trip } from 'src/app/interfaces/trip';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent {
+  constructor( ){}
+  @Input() trip!: any;
+
+  avatar:string | undefined;
+  image:string | undefined;
+
+  getImageAsBase64(): string {
+    let binary = '';
+    const bytes = new Uint8Array(this.trip.img.data.data);
+    
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);
+  }
+
+  getAvatarAsBase64(): string {
+    
+    let binary = '';
+    const bytes = new Uint8Array(this.trip._ownerId.img.data.data);
+    
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);
+  }
+
+}
