c00c1f2bda634ae19394535cbe5deb2ff755e493
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-02-28 00:22:13 +0200

    image service

---
+++ client/src/app/services/image.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+  constructor() { }
+
+  getImageAsBase64(file: any): string {
+    let binary = '';
+    const bytes = new Uint8Array(file);
+
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);
+  }
+}
