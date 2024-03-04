6a62a1ca74b5f69c122c7f83a890b9bc8072a6d1
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-03-04 13:17:05 +0200

    search update

--- client/src/app/views/search/search.component.ts
+++ client/src/app/views/search/search.component.ts
@@ -15,15 +15,29 @@ export class SearchComponent implements OnInit, OnDestroy {
   subscriptions: Subscription = new Subscription();
 
   ngOnInit() {
-    const results = this.searchService.currentPage$.subscribe((currentPage) => {
+    const results$ = this.searchService.currentPage$.subscribe((currentPage) => {
+      console.log(this.searchService.currentPage$);
        this.searchService.getSearch().subscribe({
         next: (trips: Trip[]) => {
           this.searchService.searchResult = trips;
+          console.log(trips);
         },
         error: (error: { error: { message: any; }; }) => {
+          console.log(error.error.message);
         }
       });
     });
+    this.subscriptions.add(results$);
+  }
+
+  nextPage() {
+    this.searchService.currentPage$.next(this.searchService.currentPage$.value + 1);
+  }
+
+  previousPage() {
+    if (this.searchService.currentPage$.value > 1) {
+      this.searchService.currentPage$.next(this.searchService.currentPage$.value - 1);
+    }
   }
 
   ngOnDestroy(): void {
